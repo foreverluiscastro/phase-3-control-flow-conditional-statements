97e592002b107c8bfdd00e21eb39e931e33aa4c8
Luis <foreverluiscastro@gmail.com>
2022-07-28 15:55:54 -0400

    commit tree 8400883d30a4016165d151ff38a467a99e4de5cf

--- control_flow.rb
+++ control_flow.rb
@@ -1,5 +1,10 @@
 def admin_login(username, password)
   # your code here
+  unless ((username == "ADMIN" || username == "admin")) && (password == "12345")
+    "Access denied"
+  else 
+    "Access granted"
+  end
 end
 
 def hows_the_weather(temperature)
@@ -10,7 +15,26 @@ def fizzbuzz(num)
   # your code here
 end
 
-def calculator(operation, num1, num2)
-  # your code here
-end
+# def calculator(operation, num1, num2)
+#   case operation 
+#   when "+"  then num1 + num2
+#   when "-"  then num1 - num2
+#   when "*"  then num1 * num2
+#   when "/"  then num1 / num2
+#   else puts "Invalid operation!"
+#   end
+# end
 
+def calculator(operation, num1, num2)
+  case operation
+  when "+" 
+     num1 + num2
+  when "-"  
+     num1 - num2
+  when "*"  
+     num1 * num2
+  when "/"  
+     num1 / num2
+  else "Invalid operation!"
+  end
+end
